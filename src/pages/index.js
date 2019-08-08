8fbe2a841af56766e2a06e7bb725eb58fc086de7
Tom Gooden <good3n@gmail.com>
2019-08-08 00:10:06 -0400

    content updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,36 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
+const HomeWrapper = styled.section`
+
+  div {
+    margin-bottom: 20px;
+    display: none;
+
+    span {
+      font-weight: 700;
+    }
+  }
+`
 
 const IndexPage = () => (
   <Layout>
     <SEO title="The Dirty Dozen | Fantasy Football League" />
-    <div>Teams: 12</div>
-    <div>Current Champ: Blah</div>
-    <div>Platform: Yahoo</div >
-    <div>Roster Positions: QB, WR, WR, RB, RB, TE, W / R / T, K, DEF, BN, BN, BN, BN, BN, IR</div>
-    <div>Playoffs: Week 14, 15, and 16 (6 teams)</div>
-    <div>Playoff Tie-Breaker:	Best regular season record vs opponent wins</div>
-    <div>Regular Season Tie-Breaker: None</div>
-    <div>Divisions:	Hello No</div>
-    <div>Trade Review: Commissioner Veto</div>
+    <h1>The Dirty Dozen - Fantasy Football League</h1>
+    <p>Under construction.</p>
+    <HomeWrapper>
+      <div>Reigning Champion: <span>Lenny Baily</span></div>
+      <div>Teams: <span>12</span></div>
+      <div>Platform: <span>Yahoo</span></div>
+      <div>Roster Positions: <span>QB, WR, WR, RB, RB, TE, W / R / T, K, DEF, BN, BN, BN, BN, BN, IR</span></div>
+      <div>Playoffs: <span>Week 14, 15, and 16 (6 teams)</span></div>
+      <div>Playoff Tie-Breaker:	<span>Best regular season record vs opponent wins</span></div>
+      <div>Regular Season Tie-Breaker: <span>None</span></div>
+      <div>Divisions:	<span>Hello No</span></div>
+      <div>Trade Review: <span>Commissioner Veto</span></div>
+    </HomeWrapper>
   </Layout>
 )
 
