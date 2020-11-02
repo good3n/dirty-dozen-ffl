b52d5f8cbbd93bbb043dea7bee84d83163d649d6
good3n <good3n@gmail.com>
2020-11-02 13:11:03 -0500

    add information to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,11 @@ import SEO from "../components/seo"
 import styled from "styled-components"
 
 const HomeWrapper = styled.section`
+  display: block;
+  margin-top: 50px;
 
   div {
-    margin-bottom: 20px;
-    display: none;
+    margin-bottom: 10px;
 
     span {
       font-weight: 700;
@@ -19,17 +20,36 @@ const IndexPage = () => (
   <Layout>
     <SEO title="The Dirty Dozen | Fantasy Football League" />
     <h1>The Dirty Dozen - Fantasy Football League</h1>
-    <p>Under construction.</p>
     <HomeWrapper>
-      <div>Reigning Champion: <span>Lenny Baily</span></div>
-      <div>Teams: <span>12</span></div>
-      <div>Platform: <span>Yahoo</span></div>
-      <div>Roster Positions: <span>QB, WR, WR, RB, RB, TE, W / R / T, K, DEF, BN, BN, BN, BN, BN, IR</span></div>
-      <div>Playoffs: <span>Week 14, 15, and 16 (6 teams)</span></div>
-      <div>Playoff Tie-Breaker:	<span>Best regular season record vs opponent wins</span></div>
-      <div>Regular Season Tie-Breaker: <span>None</span></div>
-      <div>Divisions:	<span>Hello No</span></div>
-      <div>Trade Review: <span>Commissioner Veto</span></div>
+      <div>
+        Reigning Champion: <span>Erik Van Fossen</span>
+      </div>
+      <div>
+        Teams: <span>12</span>
+      </div>
+      <div>
+        Platform: <span>Yahoo</span>
+      </div>
+      <div>
+        Roster Positions:{" "}
+        <span>
+          QB, WR, WR, RB, RB, TE, W / R / T, K, DEF, BN, BN, BN, BN, BN, IR
+        </span>
+      </div>
+      <div>
+        Playoffs: <span>Week 14, 15, and 16 (6 teams)</span>
+      </div>
+      <div>
+        Playoff Tie-Breaker:{" "}
+        <span>Best regular season record vs opponent wins</span>
+      </div>
+      <div>
+        Regular Season Tie-Breaker:{" "}
+        <span>Defense points, followed by kicker</span>
+      </div>
+      <div>
+        Divisions: <span>None</span>
+      </div>
     </HomeWrapper>
   </Layout>
 )
